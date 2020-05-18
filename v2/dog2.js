d2d21c1146624221fcd0a608c1c8972da2180ed6
Adam Smith <coloradoasmith@gmail.com>
2020-05-18 15:32:20 -0600

    dogv2 is working

--- v2/dog2.js
+++ v2/dog2.js
@@ -1,27 +1,38 @@
 "use strict";
 
-function getDogImages() {
-  const api = "https://dog.ceo/api/breeds/image/random/";
-  let input = document.getElementById("howMany").value;
-  let url = api + input;
-  console.log(url);
-  fetch(url)
-    .then((response) => response.json())
-    .then((responseJson) => console.log(responseJson))
-    .catch((error) => alert("Something went wrong. Try again later."));
+function getDogImages(num) {
+  if (num < 3) {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((response) => response.json())
+      .then((responseJson) => displayResults(responseJson))
+      .catch((error) => alert("Something went wrong. Try again later."));
+  } else if (num > 50) {
+    return alert("Please choose a number equal to or less than 50");
+  } else {
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+      .then((response) => response.json())
+      .then((responseJson) => displayResults(responseJson))
+      .catch((error) => alert("Something went wrong. Try again later."));
+  }
 }
 
-//function howMany() {
-//const api = "https://dog.ceo/api/breeds/image/random/";
-//let input = document.getElementById("howMany");
-//let url = api + input;
-//console.log(url);
-//}
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //clears results section
+  $(".results").html("");
+  // take every image from the response message array and append a new image to the .results section
+  responseJson.message.forEach((renderedImg) => {
+    $(".results").append(`<img src="${renderedImg}" class="results">`);
+  });
+  //display the results section
+  $(".results").removeClass("hidden");
+}
 
 function watchForm() {
   $("form").submit((event) => {
     event.preventDefault();
-    getDogImages();
+    let userNum = $("#howMany").val();
+    getDogImages(userNum);
   });
 }
 
